b9873d3ee10d717c767e43b7ef1b3a94f9eeb081
bodik1987 <bsshul@gmail.com>
2025-03-09 18:57:31 +0100

    iCONS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,39 @@ import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "No-tion",
+        short_name: "No-tion",
+        description: "No-tion",
+        theme_color: "#FFF",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
